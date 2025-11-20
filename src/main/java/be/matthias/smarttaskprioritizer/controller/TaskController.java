57dde1962b6d07a02de81dafb3045ec42f8b04e9
Matthias Lannoo <matthias.lannoo@student.ehb.be>
2025-11-20 22:59:12 +0100

    task creation form added + display of the task(s)

--- src/main/java/be/matthias/smarttaskprioritizer/controller/TaskController.java
+++ src/main/java/be/matthias/smarttaskprioritizer/controller/TaskController.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/tasks")
@@ -24,5 +28,21 @@ public class TaskController {
     public String list(Model model) {
         model.addAttribute("tasks", repo.findByCompletedFalseOrderByPriorityScoreDesc());
         return "tasks/index";
+    }
+
+    @GetMapping("/new")
+    public String newTask(Model model) {
+        model.addAttribute("task", new Task());
+        return "tasks/form";
+    }
+
+    @PostMapping
+    public String create(@Valid @ModelAttribute("task") Task task,
+                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "tasks/form";
+        }
+        service.save(task);
+        return "redirect:/tasks";
     }
 }
