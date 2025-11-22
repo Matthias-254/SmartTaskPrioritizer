3b06cef7eae8eace471c0ddfda91f231cc0f7c1d
Matthias-254 <lannoo.matthias@hotmail.com>
2025-11-22 14:27:35 +0100

    login page added with security

--- src/main/java/be/matthias/smarttaskprioritizer/config/SecurityConfig.java
+++ src/main/java/be/matthias/smarttaskprioritizer/config/SecurityConfig.java
@@ -3,6 +3,10 @@ package be.matthias.smarttaskprioritizer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,11 +14,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/login", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/tasks", true)
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout"));
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
